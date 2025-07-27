75a7ef442752a8c5511852e838bc86a11cee5a1f
Scozzi <scozzi.sol@gmail.com>
2025-07-27 00:20:41 -0400

    Fix: Correct metadata URI to use master branch

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,7 +1,7 @@
 // Off-chain metadata - Single Wedding NFT
 // Replace YOUR_USERNAME and YOUR_REPO with your actual GitHub info
 const WEDDING_NFT_URI =
-  "https://raw.githubusercontent.com/Scozzi/monke-nuptial-mass/main/public/metadata/wedding-nft.json";
+  "https://raw.githubusercontent.com/Scozzi/monke-nuptial-mass/master/public/metadata/wedding-nft.json";
 
 // Return the single wedding NFT metadata URI
 export function getWeddingNFTUri() {
